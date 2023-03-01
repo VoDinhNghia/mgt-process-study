5bf9a65673749c9ec63e8727d9576c380c158cb8
VoDinhNghia <vodinhnghia85@gmail.com>
2023-03-01 09:28:03 +0700

    Refactor code validate subject register

--- src/modules/study-process/study-process.service.ts
+++ src/modules/study-process/study-process.service.ts
@@ -46,25 +46,11 @@ export class StudyProcessService {
     return result;
   }
 
-  async validateSubject(subjectId: string): Promise<void> {
-    const subjectInfo = await this.db
-      .collection('subjects')
-      .findOne({ _id: new Types.ObjectId(subjectId), status: true });
-    if (!subjectInfo) {
-      new CommonException(404, 'Subject not found.');
-    }
-    const getAllRegister = await this.subjectSchema.find({
-      subject: new Types.ObjectId(subjectId),
-    });
-    if (subjectInfo.size <= getAllRegister.length) {
-      new CommonException(400, 'Sufficient number of students have registered');
-    }
-  }
-
   async createSubjectRegister(
     subjectDto: CreateStudySubjectProcessDto,
   ): Promise<SubjectRegisters> {
-    await this.validateSubject(subjectDto.subject);
+    const { subject, studyprocess } = subjectDto;
+    await this.validateSubject(subject, studyprocess);
     const result = await new this.subjectSchema(subjectDto).save();
     return result;
   }
@@ -72,5 +58,39 @@ export class StudyProcessService {
   calculateAccumulatedPoint(point: number, percent: number) {
     const accumulatedPoint = (point * percent) / 100;
     return accumulatedPoint;
+  }
+
+  async validateSubject(
+    subjectId: string,
+    studyprocess: string,
+  ): Promise<void> {
+    await this.validateNumberRegister(subjectId);
+    const result = await this.subjectSchema.findOne({
+      subject: new Types.ObjectId(subjectId),
+      studyprocess: new Types.ObjectId(studyprocess),
+    });
+    if (result) {
+      new CommonException(409, 'You have registed this subject already.');
+    }
+  }
+
+  async findSubjectById(subjectId: string): Promise<Record<string, any>> {
+    const result = await this.db
+      .collection('subjects')
+      .findOne({ _id: new Types.ObjectId(subjectId), status: true });
+    if (!result) {
+      new CommonException(404, 'Subject not found.');
+    }
+    return result;
+  }
+
+  async validateNumberRegister(subjectId: string): Promise<void> {
+    const subjectInfo = await this.findSubjectById(subjectId);
+    const getAllRegister = await this.subjectSchema.find({
+      subject: new Types.ObjectId(subjectId),
+    });
+    if (subjectInfo.size <= getAllRegister.length) {
+      new CommonException(409, 'Sufficient number of students have registered');
+    }
   }
 }
